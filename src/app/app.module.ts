011a98d974e65804aadd8af5ebc7c9f5f0e25ed9
Ana Azevedo <ana_azevedo_14@hotmail.com>
2019-02-10 17:41:29 +0000

    workshop initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesPageComponent } from './games-page/games-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesPageComponent
   ],
   imports: [
     BrowserModule,
